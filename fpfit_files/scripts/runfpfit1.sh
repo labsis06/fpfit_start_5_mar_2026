e4277ffcdc11f16ae6c0723ae75a1c60273e8a76
labsis06 <102295090+labsis06@users.noreply.github.com>
2026-03-24 17:08:06 +0100

    corr

--- fpfit_files/scripts/runfpfit1.sh
+++ fpfit_files/scripts/runfpfit1.sh
@@ -62,7 +62,8 @@ case "$mode" in
     # costruzione input Hypo71 corretto
 cp "${HYPO71_DIR}/flegrei.sta" HYPO71PC.INP
 
-   "${nome}.p01" >> HYPO71PC.INP
+sed -e 's/\r$//' \
+     "${nome}.p01" >> HYPO71PC.INP
 
 printf '\n' >> HYPO71PC.INP
 
